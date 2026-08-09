ce4c6759208158d3bba89402bff28618f1adb710
Aryan Tripathi <aryantripathi639470@gmail.com>
2026-08-10 00:39:03 +0530

    just basic

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+const app = express()
+dotenv.config()
+app.use(cors())
+
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log(`Server has started at port ${port}`)
+})
