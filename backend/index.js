33f55c166fc28680f88f624d502ac5b2737243c5
Aryan Tripathi <aryantripathi639470@gmail.com>
2026-08-13 23:45:21 +0530

    have don database connection

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import mongoose from "mongoose"
+import { connectDB } from "./config/db.js"
 
 const app = express()
 dotenv.config()
 app.use(cors())
 
-
-
-
 const port = process.env.PORT || 5000
 
 app.listen(port,()=>{
+    connectDB()
     console.log(`Server has started at port ${port}`)
 })
